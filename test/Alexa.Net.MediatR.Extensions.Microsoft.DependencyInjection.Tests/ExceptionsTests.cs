0bf4031936035986b0df0fdd88d5c7c709ca24f2
Nicholas Cipollina <ncipollina@captechconsulting.com>
2022-04-23 20:49:32 -0400

    Initial checkin

---
+++ test/Alexa.Net.MediatR.Extensions.Microsoft.DependencyInjection.Tests/ExceptionsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Alexa.Net.MediatR.Options;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Alexa.Net.MediatR.Extensions.Microsoft.DependencyInjection.Tests;
+
+public class ExceptionsTests
+{
+    [Fact]
+    public void AddSkillMediatorFromAssemblies_WithNullServices_ThrowsException()
+    {
+        var services = (IServiceCollection)null;
+
+        Action act = () =>
+            services.AddSkillMediatorFromAssemblies(Enumerable.Empty<Assembly>(), GetDefaultConfiguration(), "");
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AddSkillMediatorFromAssemblies_WithNullConfiguration_ThrowsException()
+    {
+        var services = GetDefaultServiceCollection();
+
+        Action act = () => services.AddSkillMediatorFromAssemblies(Enumerable.Empty<Assembly>(), null, "");
+        
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AddSkillMediatorFromAssemblies_WithNullSettingsAction_ThrowsException()
+    {
+        var services = GetDefaultServiceCollection();
+
+        Action act = () =>
+            services.AddSkillMediatorFromAssemblies(Enumerable.Empty<Assembly>(), (Action<AlexaSkillOptions>)null);
+        
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AddSkillMediatorFromAssemblies_WithNullConfigSection_ThrowsException()
+    {
+        var services = GetDefaultServiceCollection();
+
+        Action act = () =>
+            services.AddSkillMediatorFromAssemblies(Enumerable.Empty<Assembly>(), (IConfigurationSection)null);
+        
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void AddSkillMediatorFromAssemblies_WithEmptyAssemblies_ThrowsException()
+    {
+        var services = GetDefaultServiceCollection();
+        var configuration = GetDefaultConfiguration();
+
+        Action act = () => services.AddSkillMediatorFromAssemblies(Enumerable.Empty<Assembly>(), configuration, "");
+        
+        act.Should().Throw<ArgumentException>();
+    }
+
+    private static IServiceCollection GetDefaultServiceCollection()
+    {
+        var services = new ServiceCollection();
+        return services;
+    }
+    private static IConfiguration GetDefaultConfiguration()
+    {
+        var configuration = new Dictionary<string, string>();
+        return new ConfigurationBuilder().AddInMemoryCollection(configuration).Build();
+    }
+}
---
+++ test/Alexa.Net.MediatR.Extensions.Microsoft.DependencyInjection.Tests/Handlers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Alexa.Net.MediatR.Pipeline;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+
+namespace Alexa.Net.MediatR.Extensions.Microsoft.DependencyInjection.Tests;
+
+public class LaunchRequestHandler : IRequestHandler<LaunchRequest>
+{
+    public Task<SkillResponse> Handle(IHandlerInput input, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return input.ResponseBuilder.Empty(cancellationToken);
+    }
+}
+
+public class SessionEndHandler : IRequestHandler<SessionEndedRequest>
+{
+    public Task<SkillResponse> Handle(IHandlerInput input, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return input.ResponseBuilder.Empty(cancellationToken);
+    }
+}
+
+public class IntentHandler : IRequestHandler<IntentRequest>
+{
+    public Task<SkillResponse> Handle(IHandlerInput input, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return input.ResponseBuilder.Empty(cancellationToken);
+    }
+}
+
+public class ResponseInterceptor : IResponseInterceptor
+{
+    public Task Process(IHandlerInput input, SkillResponse output, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class RequestInterceptor : IRequestInterceptor
+{
+    public Task Process(IHandlerInput input, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class ExceptionHandler : IExceptionHandler
+{
+    public Task<bool> CanHandle(IHandlerInput handlerInput, Exception ex, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task<SkillResponse> Handle(IHandlerInput handlerInput, Exception ex, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return handlerInput.ResponseBuilder.Empty(cancellationToken);
+    }
+}
